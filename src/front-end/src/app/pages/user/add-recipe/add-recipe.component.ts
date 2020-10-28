03b7f9cdbb4bbf81376aacb3e7475b124ec34d64
Adriana Windmuller <adrianawindmuller@outlook.com>
2020-10-28 17:32:47 +0000

    :recycle: Refactoring forms in the recipe

--- src/front-end/src/app/pages/user/add-recipe/add-recipe.component.ts
+++ src/front-end/src/app/pages/user/add-recipe/add-recipe.component.ts
@@ -6,6 +6,8 @@ import { of } from 'rxjs';
 import { catchError, finalize } from 'rxjs/operators';
 import { Router } from '@angular/router';
 import { Recipe } from 'src/app/shared/recipe.model';
+import { Category } from 'src/app/shared/step/category.model';
+import { MatSlideToggleChange } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-add-recipe',
@@ -13,21 +15,23 @@ import { Recipe } from 'src/app/shared/recipe.model';
   styleUrls: ['./add-recipe.component.sass'],
 })
 export class AddRecipeComponent implements OnInit {
-  stepBasic: FormGroup;
-  stepIngredients: FormGroup;
-  stepAdvanced: FormGroup;
-  notEditRecipe: boolean = false;
-  imageUrl: string[] = [];
+  recipeForm: FormGroup;
+  images: string[] = [];
+  publicado: boolean;
+  category: Category[];
 
   constructor(
     private fb: FormBuilder,
     private recipeService: RecipeService,
     private toastr: ToastrService,
-    private route: Router
+    private route: Router,
+    private service: RecipeService
   ) {}
 
   ngOnInit(): void {
-    this.stepBasic = this.fb.group({
+    this.service.getCategory().subscribe((res) => (this.category = res));
+
+    this.recipeForm = this.fb.group({
       name: [
         '',
         [
@@ -39,8 +43,6 @@ export class AddRecipeComponent implements OnInit {
       category: ['', Validators.required],
       numberPortion: ['', [Validators.required, Validators.maxLength(3)]],
       preparationTime: ['', [Validators.required, Validators.maxLength(3)]],
-    });
-    this.stepIngredients = this.fb.group({
       ingredients: [
         '',
         [
@@ -57,37 +59,19 @@ export class AddRecipeComponent implements OnInit {
           Validators.maxLength(1000),
         ],
       ],
-    });
-    this.stepAdvanced = this.fb.group({
       public: [''],
       images: ['', Validators.required],
     });
   }
 
-  invalidForm() {
-    return (
-      this.stepBasic.invalid ||
-      this.stepIngredients.invalid ||
-      this.stepAdvanced.invalid
-    );
-  }
-
-  joinForms() {
-    return {
-      ...this.stepBasic.value,
-      ...this.stepIngredients.value,
-      ...this.stepAdvanced.value,
-    };
-  }
-
   createRecipe() {
-    if (this.invalidForm()) {
+    if (!this.recipeForm.valid) {
       return;
     }
-    const newRecipe = this.joinForms();
+    const newRecipe = this.recipeForm.value;
 
     let recipeModel = newRecipe as Recipe;
-    recipeModel.images = this.imageUrl;
+    recipeModel.images = this.images;
 
     this.recipeService
       .createNewRecipe(recipeModel)
@@ -110,6 +94,70 @@ export class AddRecipeComponent implements OnInit {
   }
 
   changedFiles(file: string[]) {
-    this.imageUrl = file;
+    this.images = file;
+  }
+
+  changePublic(event: MatSlideToggleChange) {
+    this.publicado = event.checked;
+  }
+
+  fileUploadImage(event) {
+    var files = event.target.files;
+
+    if (files) {
+      for (let file of files) {
+        let reader = new FileReader();
+        reader.onload = (e: any) => {
+          this.images.push(e.target.result);
+        };
+        reader.readAsDataURL(file);
+      }
+    }
+  }
+
+  resetImage(i) {
+    this.images.splice(i, 1);
+  }
+
+  fileUploadEditImage(event) {
+    var files = event.target.files;
+
+    if (files) {
+      for (let file of files) {
+        let reader = new FileReader();
+        reader.onload = (e: any) => {
+          this.images.push(e.target.result);
+        };
+        reader.readAsDataURL(file);
+      }
+    }
+  }
+
+  resetEditImage(i) {
+    this.images.splice(i, 1);
+  }
+
+  mensagemErroName() {
+    return this.recipeForm.get('name').hasError('required')
+      ? 'Campo Obrigatório'
+      : this.recipeForm.get('name').hasError('minlength')
+      ? 'Insira pelo menos 3 caracteres'
+      : '';
+  }
+
+  mensagemErroIngredients() {
+    return this.recipeForm.get('ingredients').hasError('required')
+      ? 'Campo Obrigatório'
+      : this.recipeForm.get('ingredients').hasError('minlength')
+      ? 'Insira pelo menos 3 caracteres'
+      : '';
+  }
+
+  mensagemErroPreparation() {
+    return this.recipeForm.get('preparationMode').hasError('required')
+      ? ' Campo Obrigatório'
+      : this.recipeForm.get('preparationMode').hasError('minlength')
+      ? 'Insira pelo menos 3 caracteres'
+      : '';
   }
 }
