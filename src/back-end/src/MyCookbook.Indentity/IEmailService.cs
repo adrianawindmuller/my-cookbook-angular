26a1beade1c1755df47c98fc96dd58eb2335dca0
Adriana Windmuller <adrianawindmuller@outlook.com>
2021-09-29 17:12:48 +0100

    removed project IdentityServer

--- src/back-end/src/MyCookbook.Indentity/IEmailService.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace MyCookbook.Indentity
-{
-    public interface IEmailService
-    {
-        Task SendEmailAsync(string emailTo, string subject, string htmlMessage);
-    }
-}
--- src/front-end/src/app/app.module.ts
+++ src/front-end/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { AppComponent } from './app.component';
 import { AppLayoutModule } from './app-layout/app-layout.module';
 import { SharedModule } from './shared/shared.module';
 import { PagesModule } from './pages/pages.module';
-
 @NgModule({
   declarations: [AppComponent],
   imports: [AppLayoutModule, SharedModule, PagesModule],
